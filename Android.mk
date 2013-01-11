e3c1c48ff5b3f0a5e963bd59025059b39ce3e285
Shao Yuru <shaoyuru@gmail.com>
2013-01-12 01:00:44 +0800

    add files

---
+++ Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	inject.c \
+	shellcode.S
+	
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_MODULE:= inject
+
+LOCAL_SHARED_LIBRARIES := libdl liblog
+
+LOCAL_STATIC_LIBRARIES := libc
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := debug
+
+include $(BUILD_EXECUTABLE)
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ droid-injectso
 ==============
 
 A shared libraries injection tool.
+
+Has been tested on Android 2.2, 2.3 and 4.1
